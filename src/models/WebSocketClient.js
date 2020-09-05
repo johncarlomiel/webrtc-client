87ef66edcede9b08f275c2be52a1152bfc63988c
John Carlo Miel <johncarlomiel@gmail.com>
2020-09-05 21:40:55 +0800

    :sparkles: Validate Media Devices (#13)
    Co-authored-by: John Carlo Miel <johncarlomiel@janan.local>

--- src/models/WebSocketClient.js
+++ src/models/WebSocketClient.js
@@ -1,12 +1,18 @@
 class WebSocketClient {
-    constructor() {
-        this.ws = new WebSocket(`wss://${process.env.REACT_APP_API_DOMAIN}`);
+  constructor() {
+    let domain = process.env.REACT_APP_API_DOMAIN;
+
+    if (process.env.NODE_ENV !== 'production') {
+      domain = '192.168.100.91:8080';
     }
 
-    sendMessage(payload) {
-        console.log(payload)
-        this.ws.send(JSON.stringify(payload));
-    }
+    this.ws = new WebSocket(`wss://${domain}`);
+  }
+
+  sendMessage(payload) {
+    console.log(payload)
+    this.ws.send(JSON.stringify(payload));
+  }
 }
 
 export default new WebSocketClient();
